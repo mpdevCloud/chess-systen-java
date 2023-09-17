7650d0e82d524363f3aba2f23d8b904fa187f258
mpdevCloud <127260024+mpdevCloud@users.noreply.github.com>
2023-09-17 18:12:26 -0300

    BoardException and defensive programing

---
+++ src/boardgame/BoardExcepton.java
@@ -0,0 +1,13 @@
+package boardgame;
+
+public class BoardExcepton extends RuntimeException{
+
+
+	private static final long serialVersionUID = 1L;
+	
+	public BoardExcepton(String msg) {
+		super(msg);		
+	}
+	
+
+}
